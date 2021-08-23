36c3955e128e0ad9ad4c047e351e2f7ada774646
mdberkey <meberkey@wisc.edu>
2021-08-22 21:10:15 -0500

    Fixed run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-import subprocess
+from app import create_app
 
 if __name__ == '__main__':
-    subprocess.run('cd app')
-    subprocess.run('export FLASK_APP=app')
-    subprocess.run('flask run')
+    app = create_app()
+    app.run()
